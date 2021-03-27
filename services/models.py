c664e7c855c384c078eb562f69bec081e52c8224
Andrey Glazunov <andrew.glaz@yandex.ru>
2021-03-27 20:24:51 +0300

    Фикс модалок

--- services/models.py
+++ services/models.py
@@ -8,6 +8,7 @@ class OperatingAirline(models.Model):
     iata_code = models.CharField('Код iata', max_length=255)
     icao_code = models.CharField('Код icao', max_length=255)
     name = models.CharField('Название', max_length=255)
+    services = models.ManyToManyField('Service', verbose_name='Сервисы', blank=True)
 
     class Meta:
         verbose_name = 'Авиакомпания'
@@ -25,12 +26,21 @@ class FlightNumber(models.Model):
         verbose_name_plural = 'Рейсы'
 
 
+class Category(models.Model):
+    name = models.CharField('Название', max_length=255)
+
+    class Meta:
+        verbose_name = 'Категория инвалидности'
+        verbose_name_plural = 'Категории инвалидности'
+
+
 class User(models.Model):
     """
     Пользователи
     """
     name = models.CharField('Код iata', max_length=255)
     surname = models.CharField('Код iata', max_length=255)
+    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = 'Пользователь'
@@ -42,3 +52,15 @@ class Service(models.Model):
     Услуга
     """
     name = models.CharField('Код iata', max_length=255)
+    surname = models.CharField('Код iata', max_length=255)
+    user = models.ForeignKey(User, verbose_name="Пользователь", on_delete=models.CASCADE)
+    meeting_time = models.DateTimeField("Дата встречи", blank=True, null=True)
+    place = models.CharField("Место встречи", max_length=255)
+    escort_features = models.CharField("Особенности сопровождения", max_length=255)
+
+    class Meta:
+        verbose_name = 'Услуга'
+        verbose_name_plural = 'Услуги'
+
+
+# class
